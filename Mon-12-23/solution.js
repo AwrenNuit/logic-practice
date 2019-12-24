6d11ee286505b2934feba3ce36df3a3af7f3dc15
AwrenNuit <a.rennaker@yahoo.com>
2019-12-23 19:50:37 -0600

    added day 1

---
+++ Mon-12-23/solution.js
@@ -0,0 +1,9 @@
+let dividesEvenly=(a, b)=>{return a%b===0?true:false}
+
+console.log('6,4:', dividesEvenly(6,4));
+console.log('10,5:', dividesEvenly(10,5));
+console.log('2,2:', dividesEvenly(2,2));
+console.log('132,36:', dividesEvenly(132,36));
+console.log('98,7:', dividesEvenly(98,7));
+console.log('85,4:', dividesEvenly(85,4));
+
