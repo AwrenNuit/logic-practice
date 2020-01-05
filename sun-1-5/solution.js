33d7c2e63119053e1f31cf8c5bf4ac35e0a7503b
AwrenNuit <a.rennaker@yahoo.com>
2020-01-05 12:03:32 -0600

    added day 14

---
+++ sun-1-5/solution.js
@@ -0,0 +1,7 @@
+const timeForMilkAndCookies = date => date.getMonth() === 11 && date.getDate() === 24;
+
+console.log(timeForMilkAndCookies(new Date(2013, 11, 24)));
+console.log(timeForMilkAndCookies(new Date(3000, 11, 24)));
+console.log(timeForMilkAndCookies(new Date(2154, 11, 11)));
+console.log(timeForMilkAndCookies(new Date(2010, 11, 2)));
+console.log(timeForMilkAndCookies(new Date(1980, 9, 24)));
